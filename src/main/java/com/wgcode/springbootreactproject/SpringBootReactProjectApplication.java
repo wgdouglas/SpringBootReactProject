84a6eacd7729955fc4802920eb1e90522affdf23
gfizzle <winstongdouglas4@gmail.com>
2023-11-26 22:26:24 -0600

    Master branch set up for full-stack project

---
+++ src/main/java/com/wgcode/springbootreactproject/SpringBootReactProjectApplication.java
@@ -0,0 +1,13 @@
+package com.wgcode.springbootreactproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootReactProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootReactProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wgcode/springbootreactproject/SpringBootReactProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wgcode.springbootreactproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootReactProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
